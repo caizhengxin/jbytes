a1d2a0ae55b80ed4a77f35d14de8460a64c6da80
jankincai <jankincai12@gmail.com>
2024-12-17 14:04:13 +0800

    :hammer: refactor(upgrade): update `BufWrite` trait type.

--- src/traits.rs
+++ src/traits.rs
@@ -104,54 +104,177 @@ pub trait BufRead {
 pub trait BufWrite {
     fn push<V: AsRef<[u8]>>(&mut self, value: V);
 
-    fn push_char(&mut self, value: char);
+    #[inline]
+    fn push_bytes(&mut self, value: &[u8]) {
+        self.push(value)
+    }
 
-    fn push_bytes(&mut self, value: &[u8]);
+    #[inline]
+    fn push_char(&mut self, value: char) {
+        self.push([value as u8]);
+    }
 
-    fn push_u8(&mut self, value: u8);
+    #[inline]
+    fn push_u8(&mut self, value: u8) {
+        self.push([value])
+    }
+
+    #[inline]
+    fn push_be_u8(&mut self, value: u8) {
+        self.push_u8(value)
+    }
+
+    #[inline]
+    fn push_le_u8(&mut self, value: u8) {
+        self.push_u8(value)
+    }
+
+    #[inline]
+    fn push_ne_u8(&mut self, value: u8) {
+        self.push_u8(value)
+    }
 
     #[inline]
     fn push_u16(&mut self, value: u16) {
-        self.push_be_u16(value);
+        self.push(value.to_be_bytes())
     }
 
-    fn push_be_u16(&mut self, value: u16);
+    #[inline]
+    fn push_be_u16(&mut self, value: u16) {
+        self.push(value.to_be_bytes())
+    }
 
-    fn push_le_u16(&mut self, value: u16);
+    #[inline]
+    fn push_le_u16(&mut self, value: u16) {
+        self.push(value.to_le_bytes())
+    }
+
+    #[inline]
+    fn push_ne_u16(&mut self, value: u16) {
+        self.push(value.to_ne_bytes())
+    }
 
     #[inline]
     fn push_u24(&mut self, value: u32) {
         self.push_be_u24(value);
     }
 
-    fn push_be_u24(&mut self, value: u32);
+    #[inline]
+    fn push_be_u24(&mut self, value: u32) {
+        self.push(&value.to_be_bytes()[1..])
+    }
 
-    fn push_le_u24(&mut self, value: u32);
+    #[inline]
+    fn push_le_u24(&mut self, value: u32) {
+        self.push(&value.to_le_bytes()[..3])
+    }
+
+    #[inline]
+    fn push_ne_u24(&mut self, value: u32) {
+        if cfg!(target_endian = "big") {
+            self.push_be_u24(value)
+        } else {
+            self.push_le_u24(value)
+        }
+    }
 
     #[inline]
     fn push_u32(&mut self, value: u32) {
-        self.push_be_u32(value);
+        self.push(value.to_be_bytes())
     }
 
-    fn push_be_u32(&mut self, value: u32);
+    #[inline]
+    fn push_be_u32(&mut self, value: u32) {
+        self.push(value.to_be_bytes())
+    }
 
-    fn push_le_u32(&mut self, value: u32);
+    #[inline]
+    fn push_le_u32(&mut self, value: u32) {
+        self.push(value.to_le_bytes())
+    }
+
+    #[inline]
+    fn push_ne_u32(&mut self, value: u32) {
+        self.push(value.to_ne_bytes())
+    }
 
     #[inline]
     fn push_u64(&mut self, value: u64) {
-        self.push_be_u64(value);
+        self.push(value.to_be_bytes())
     }
 
-    fn push_be_u64(&mut self, value: u64);
+    #[inline]
+    fn push_be_u64(&mut self, value: u64) {
+        self.push(value.to_be_bytes())
+    }
 
-    fn push_le_u64(&mut self, value: u64);
+    #[inline]
+    fn push_le_u64(&mut self, value: u64) {
+        self.push(value.to_le_bytes())
+    }
+
+    #[inline]
+    fn push_ne_u64(&mut self, value: u64) {
+        self.push(value.to_ne_bytes())
+    }
 
     #[inline]
     fn push_u128(&mut self, value: u128) {
-        self.push_be_u128(value);
+        self.push(value.to_be_bytes())
     }
 
-    fn push_be_u128(&mut self, value: u128);
+    #[inline]
+    fn push_be_u128(&mut self, value: u128) {
+        self.push(value.to_be_bytes())
+    }
 
-    fn push_le_u128(&mut self, value: u128);
+    #[inline]
+    fn push_le_u128(&mut self, value: u128) {
+        self.push(value.to_le_bytes())
+    }
+
+    #[inline]
+    fn push_ne_u128(&mut self, value: u128) {
+        self.push(value.to_ne_bytes())
+    }
+
+    #[inline]
+    fn push_f32(&mut self, value: f32) {
+        self.push(value.to_be_bytes())
+    }
+
+    #[inline]
+    fn push_be_f32(&mut self, value: f32) {
+        self.push(value.to_be_bytes())
+    }
+
+    #[inline]
+    fn push_le_f32(&mut self, value: f32) {
+        self.push(value.to_le_bytes())
+    }
+
+    #[inline]
+    fn push_ne_f32(&mut self, value: f32) {
+        self.push(value.to_ne_bytes())
+    }
+
+    #[inline]
+    fn push_f64(&mut self, value: f64) {
+        self.push(value.to_be_bytes())
+    }
+
+    #[inline]
+    fn push_be_f64(&mut self, value: f64) {
+        self.push(value.to_be_bytes())
+    }
+
+    #[inline]
+    fn push_le_f64(&mut self, value: f64) {
+        self.push(value.to_le_bytes())
+    }
+
+    #[inline]
+    fn push_ne_f64(&mut self, value: f64) {
+        self.push(value.to_ne_bytes())
+    }
 }
