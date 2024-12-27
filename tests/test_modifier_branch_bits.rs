cf849177af5616a9b016371a866f0e135af304b9
jankincai <caizhengxin@bolean.com.cn>
2024-12-27 17:54:07 +0800

    :white_check_mark: test(upgrade): update tests code.

--- tests/test_modifier_branch_bits.rs
+++ tests/test_modifier_branch_bits.rs
@@ -6,15 +6,16 @@ use jbytes::prelude::*;
 struct BranchExample {
     pub cmd: u16,
     #[jbytes(branch="cmd")]
-    pub body: BranchRangeExampleBody,
+    pub body: BranchBitsExampleBody,
 }
 
 
 #[derive(Debug, PartialEq, Eq, ByteEncode, ByteDecode)]
-enum BranchRangeExampleBody {
-    #[jbytes(branch_range="1..=3")]
-    V0,                               // Set 1,2,3
-    V1 {                              // Increment to 4
+enum BranchBitsExampleBody {
+    #[jbytes(branch_bits=0x0001)]    // xxxx xxxx xxxx xxx1
+    V0,
+    #[jbytes(branch_bits=0x0002)]    // xxxx xxxx xxxx xx10
+    V1 {
         a: u8,
         b: u16,
     },
@@ -24,13 +25,13 @@ enum BranchRangeExampleBody {
 
 
 #[test]
-fn test_modifier_branch_range() {
+fn test_modifier_branch_bits() {
     // cmd = 1
     let data = [
         0x00, 0x01,                   // cmd
     ];
     let bytes = Bytes::new(data);
-    let value = BranchExample { cmd: 1, body: BranchRangeExampleBody::V0 };
+    let value = BranchExample { cmd: 1, body: BranchBitsExampleBody::V0 };
     assert_eq!(BranchExample::decode(&bytes).unwrap(), value);
     assert_eq!(bytes.remaining_len(), 0);
 
@@ -39,9 +40,11 @@ fn test_modifier_branch_range() {
     // cmd = 2
     let data = [
         0x00, 0x02,                   // cmd
+        0x01,                         // a
+        0x00, 0x02,                   // b
     ];
     let bytes = Bytes::new(data);
-    let value = BranchExample { cmd: 2, body: BranchRangeExampleBody::V0 };
+    let value = BranchExample { cmd: 2, body: BranchBitsExampleBody::V1 { a: 1, b: 2 } };
     assert_eq!(BranchExample::decode(&bytes).unwrap(), value);
     assert_eq!(bytes.remaining_len(), 0);
 
@@ -52,20 +55,20 @@ fn test_modifier_branch_range() {
         0x00, 0x03,                   // cmd
     ];
     let bytes = Bytes::new(data);
-    let value = BranchExample { cmd: 3, body: BranchRangeExampleBody::V0 };
+    let value = BranchExample { cmd: 3, body: BranchBitsExampleBody::V0 };
     assert_eq!(BranchExample::decode(&bytes).unwrap(), value);
     assert_eq!(bytes.remaining_len(), 0);
 
     assert_eq!(*jbytes::encode(value).unwrap(), data);
 
-    // cmd = 4
+    // cmd = 0x000a
     let data = [
-        0x00, 0x04,                   // cmd
+        0x00, 0x0a,                   // cmd
         0x01,                         // a
         0x00, 0x02,                   // b
     ];
     let bytes = Bytes::new(data);
-    let value = BranchExample { cmd: 4, body: BranchRangeExampleBody::V1 { a: 1, b: 2 } };
+    let value = BranchExample { cmd: 0x000a, body: BranchBitsExampleBody::V1 { a: 1, b: 2 } };
     assert_eq!(BranchExample::decode(&bytes).unwrap(), value);
     assert_eq!(bytes.remaining_len(), 0);
 
@@ -73,20 +76,20 @@ fn test_modifier_branch_range() {
 
     // Unknown
     let data = [
-        0x00, 0x05,                   // cmd
+        0x00, 0x50,                   // cmd
     ];
     let bytes = Bytes::new(data);
-    let value = BranchExample { cmd: 5, body: BranchRangeExampleBody::Unknown};
+    let value = BranchExample { cmd: 0x50, body: BranchBitsExampleBody::Unknown};
     assert_eq!(BranchExample::decode(&bytes).unwrap(), value);
     assert_eq!(bytes.remaining_len(), 0);
 
     assert_eq!(*jbytes::encode(value).unwrap(), data);
 
     let data = [
-        0x00, 0x1f,                   // cmd
+        0x00, 0xf0,                   // cmd
     ];
     let bytes = Bytes::new(data);
-    let value = BranchExample { cmd: 0x1f, body: BranchRangeExampleBody::Unknown};
+    let value = BranchExample { cmd: 0xf0, body: BranchBitsExampleBody::Unknown};
     assert_eq!(BranchExample::decode(&bytes).unwrap(), value);
     assert_eq!(bytes.remaining_len(), 0);
 
