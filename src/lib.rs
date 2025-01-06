ace9afe1c12a521250c1488d8c7d1a920ac74268
jankincai <caizhengxin@bolean.com.cn>
2025-01-06 17:28:15 +0800

    :hammer: refactor(upgrade): update code and docs.

--- src/lib.rs
+++ src/lib.rs
@@ -58,18 +58,142 @@ pub mod prelude {
 }
 
 
+/// This is a decode function of byte stream.
+/// 
+/// # Example
+/// 
+/// ```
+/// use jbytes_derive::{ByteEncode, ByteDecode};
+///
+///
+/// #[derive(Debug, PartialEq, Eq, ByteEncode, ByteDecode)]
+/// pub struct SimpleExample {
+///    pub length: u16,
+///    #[jbytes(length="length")]
+///    pub value: String,
+///    pub cmd: u8,
+///    #[jbytes(branch="cmd")]
+///    pub body: SimpleExampleBody,
+/// }
+///
+///
+/// #[derive(Debug, PartialEq, Eq, ByteEncode, ByteDecode)]
+/// pub enum SimpleExampleBody {
+///     #[jbytes(branch_value=1)]
+///     Read {
+///         address: u8,
+///     },
+///     Write {
+///         address: u8,
+///         value: [u8; 3],
+///     },
+///     #[jbytes(branch_default)]
+///     Unknown, 
+/// }
+///
+///
+/// fn main() {
+///     let input = b"\x00\x03\x31\x32\x33\x01\x05";
+///     let value: SimpleExample = jbytes::decode(input).unwrap();
+///     assert_eq!(value, SimpleExample { length: 3, value: "123".to_string(), cmd: 1, body: SimpleExampleBody::Read { address: 5 } });
+///     assert_eq!(*jbytes::encode(value).unwrap(), input);
+/// }
+/// ```
 #[inline]
 pub fn decode<I: AsRef<[u8]>, T: ByteDecode>(input: &I) -> JResult<T> {
     T::decode_inner(&Bytes::new(input), None, None)
 }
 
 
+/// This is a decode function of byte stream.
+/// 
+/// # Example
+/// 
+/// ```rust
+/// use jbytes_derive::{BorrowByteEncode, BorrowByteDecode};
+/// use jbytes::prelude::*;
+///
+///
+/// #[derive(Debug, Default, PartialEq, Eq, BorrowByteEncode, BorrowByteDecode)]
+/// pub struct Http<'a> {
+///     #[jbytes(linend=b"\x20")]
+///     pub method: &'a str,
+///     #[jbytes(linend=b"\x20")]
+///     pub uri: &'a str,
+///     #[jbytes(linend=b"\r\n")]
+///     pub version: &'a str,
+///     #[jbytes(try_count=20)]
+///     pub headers: Vec<HttpHeader<'a>>,
+/// }
+///
+///
+/// #[derive(Debug, Default, PartialEq, Eq, BorrowByteEncode, BorrowByteDecode)]
+/// pub struct HttpHeader<'a> {
+///     #[jbytes(linend=b": ")]
+///     pub key: &'a str,
+///     #[jbytes(linend=b"\r\n")]
+///     pub value: &'a str,
+/// }
+///
+///
+/// fn main() {
+///     // decode
+///     let data = b"GET http://www.jankincai.com/ HTTP/1.1\r\nHost: www.jankincai.com\r\nAccept-Encoding: gzip, deflate\r\n";
+///     let bytes = Bytes::new(data);
+///     let value: Http = jbytes::decode_borrow(&bytes).unwrap();
+///     println!("{value:?}");
+
+///     // encode
+///     assert_eq!(*jbytes::encode_borrow(value).unwrap(), data);
+/// }
+/// ```
 #[inline]
 pub fn decode_borrow<'de, I: AsRef<[u8]>, T: BorrowByteDecode<'de>>(input: &'de Bytes<I>) -> JResult<T> {
     T::decode_inner(input, None, None)
 }
 
 
+/// This is a encode function of byte stream.
+/// 
+/// # Example
+/// 
+/// ```
+/// use jbytes_derive::{ByteEncode, ByteDecode};
+///
+///
+/// #[derive(Debug, PartialEq, Eq, ByteEncode, ByteDecode)]
+/// pub struct SimpleExample {
+///    pub length: u16,
+///    #[jbytes(length="length")]
+///    pub value: String,
+///    pub cmd: u8,
+///    #[jbytes(branch="cmd")]
+///    pub body: SimpleExampleBody,
+/// }
+///
+///
+/// #[derive(Debug, PartialEq, Eq, ByteEncode, ByteDecode)]
+/// pub enum SimpleExampleBody {
+///     #[jbytes(branch_value=1)]
+///     Read {
+///         address: u8,
+///     },
+///     Write {
+///         address: u8,
+///         value: [u8; 3],
+///     },
+///     #[jbytes(branch_default)]
+///     Unknown, 
+/// }
+///
+///
+/// fn main() {
+///     let input = b"\x00\x03\x31\x32\x33\x01\x05";
+///     let value: SimpleExample = jbytes::decode(input).unwrap();
+///     assert_eq!(value, SimpleExample { length: 3, value: "123".to_string(), cmd: 1, body: SimpleExampleBody::Read { address: 5 } });
+///     assert_eq!(*jbytes::encode(value).unwrap(), input);
+/// }
+/// ```
 #[inline]
 pub fn encode<T: ByteEncode>(t: T) -> JResult<Buffer> {
     let mut buf = Buffer::new();
@@ -80,6 +204,48 @@ pub fn encode<T: ByteEncode>(t: T) -> JResult<Buffer> {
 }
 
 
+/// This is a encode function of byte stream.
+/// 
+/// # Example
+/// 
+/// ```rust
+/// use jbytes_derive::{BorrowByteEncode, BorrowByteDecode};
+/// use jbytes::prelude::*;
+///
+///
+/// #[derive(Debug, Default, PartialEq, Eq, BorrowByteEncode, BorrowByteDecode)]
+/// pub struct Http<'a> {
+///     #[jbytes(linend=b"\x20")]
+///     pub method: &'a str,
+///     #[jbytes(linend=b"\x20")]
+///     pub uri: &'a str,
+///     #[jbytes(linend=b"\r\n")]
+///     pub version: &'a str,
+///     #[jbytes(try_count=20)]
+///     pub headers: Vec<HttpHeader<'a>>,
+/// }
+///
+///
+/// #[derive(Debug, Default, PartialEq, Eq, BorrowByteEncode, BorrowByteDecode)]
+/// pub struct HttpHeader<'a> {
+///     #[jbytes(linend=b": ")]
+///     pub key: &'a str,
+///     #[jbytes(linend=b"\r\n")]
+///     pub value: &'a str,
+/// }
+///
+///
+/// fn main() {
+///     // decode
+///     let data = b"GET http://www.jankincai.com/ HTTP/1.1\r\nHost: www.jankincai.com\r\nAccept-Encoding: gzip, deflate\r\n";
+///     let bytes = Bytes::new(data);
+///     let value: Http = jbytes::decode_borrow(&bytes).unwrap();
+///     println!("{value:?}");
+
+///     // encode
+///     assert_eq!(*jbytes::encode_borrow(value).unwrap(), data);
+/// }
+/// ```
 #[inline]
 pub fn encode_borrow<T: BorrowByteEncode>(t: T) -> JResult<Buffer> {
     let mut buf = Buffer::new();
