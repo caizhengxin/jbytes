83e422145c1638a970dfecfa8527df952a4d96a9
jankincai <jankincai12@gmail.com>
2025-01-16 16:31:47 +0800

    :hammer: refactor(upgrade): update `Debug` function for `MacAddress` type.

--- src/types/mac_address.rs
+++ src/types/mac_address.rs
@@ -26,7 +26,7 @@ pub enum MacAddressParseError {
 /// assert_eq!(MacAddress::from_str("aa:bb:cc:dd:ee:ff").unwrap().to_bits(), 0xaabbccddeeff);
 /// assert_eq!(MacAddress::from_bits(0xaabbccddeeff).to_string(), "aa:bb:cc:dd:ee:ff");
 /// ```
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Default, Hash)]
+#[derive(PartialEq, Eq, Clone, Copy, Default, Hash)]
 pub struct MacAddress([u8; 6]);
 
 
@@ -136,6 +136,15 @@ impl fmt::Display for MacAddress {
 
         write!(f, "{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5])        
     }
+}
+
+
+impl fmt::Debug for MacAddress {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mac = self.0;
+
+        write!(f, "{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5])        
+    }   
 }
 
 
